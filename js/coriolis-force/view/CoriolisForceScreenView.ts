919df62500c15f973f9b1d94c75fc15ace47b7c0
Kaden Hart <a02366638@usu.edu>
2024-06-06 15:42:25 -0400

    rotating ice disk

--- js/coriolis-force/view/CoriolisForceScreenView.ts
+++ js/coriolis-force/view/CoriolisForceScreenView.ts
@@ -20,6 +20,8 @@ type SelfOptions = {
 type CoriolisForceScreenViewOptions = SelfOptions & ScreenViewOptions;
 
 export default class CoriolisForceScreenView extends ScreenView {
+  domNode: any;
+  puck: import("c:/Users/kaden/phetsims/chipper/node_modules/@types/three/index").Mesh<import("c:/Users/kaden/phetsims/chipper/node_modules/@types/three/index").SphereGeometry, import("c:/Users/kaden/phetsims/chipper/node_modules/@types/three/index").MeshBasicMaterial>;
 
   public constructor( model: CoriolisForceModel, providedOptions: CoriolisForceScreenViewOptions ) {
 
@@ -43,6 +45,45 @@ export default class CoriolisForceScreenView extends ScreenView {
       tandem: options.tandem.createTandem( 'resetAllButton' )
     } );
     this.addChild( resetAllButton );
+
+    const rootNode = new phet.scenery.Node();
+    const display = new phet.scenery.Display(rootNode);
+    document.body.appendChild(display.domElement);
+
+    // set up a scene with three.js
+    const scene = new THREE.Scene();
+    const camera = new THREE.OrthographicCamera(-this.layoutBounds.maxX/2,this.layoutBounds.maxX/2,-this.layoutBounds.maxY/2,this.layoutBounds.maxY/2);
+    camera.position.set(0, 0, 0)
+    camera.position.z = 1;
+    const puckGeometry = new THREE.SphereGeometry(200, 32, 32);
+    const puckMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, transparent: true, depthTest: true });
+    var iceMaterial = new THREE.MeshBasicMaterial({
+      map: new THREE.TextureLoader().load('/coriolis-force/js/coriolis-force/view/6465863.jpg'),
+      transparent: true,
+      alphaTest: 0.5,
+      side: THREE.DoubleSide
+    });
+    const puck = new THREE.Mesh(puckGeometry, iceMaterial);
+    this.puck = puck;
+    
+
+    scene.add(puck);
+    // Get the size (length, width, and height) of the bounding box
+
+    var renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+    renderer.setClearColor(0x000000, 0);
+    renderer.setClearAlpha(0);
+    renderer.setSize(this.layoutBounds.maxX, this.layoutBounds.maxY);
+    document.body.appendChild(renderer.domElement);
+
+    const domNode = new phet.scenery.DOM(renderer.domElement);
+    this.domNode = domNode;
+    
+    this.domNode._container.style.position = ""
+    this.addChild(domNode);
+    display.updateOnRequestAnimationFrame(() => {
+      renderer.render(scene, camera);
+    });
   }
 
   /**
@@ -57,7 +98,7 @@ export default class CoriolisForceScreenView extends ScreenView {
    * @param dt - time step, in seconds
    */
   public override step( dt: number ): void {
-    //TODO
+    this.puck.rotateZ(dt);
   }
 }
 
